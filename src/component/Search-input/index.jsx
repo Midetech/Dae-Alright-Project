f9e757b5f279b72c6dcc984e3ea73bf31663feee
Lawal mukhtar <lmukhtar29@gmail.com>
2020-03-30 18:02:09 -0700

    work on the search field

--- src/component/Search-input/index.jsx
+++ src/component/Search-input/index.jsx
@@ -1,30 +1,59 @@
 import React from "react"
 import "./search-input.scss"
-
-
+const lookup = {
+    "nigeria": [
+        {id: '1', place: 'Nigeria'},
+        {id: '2', place: 'Ghana'},
+        {id: '3', place: 'Usa'},
+    ],
+    "ghana": [
+        {id: '1', place: 'Kumasi'},
+        {id: '2', place: 'Ghana'},
+        {id: '3', place: 'Bida'},
+    ],
+    "usa": [
+        {id: '1', place: 'Chicago'},
+        {id: '2', place: 'New York'},
+        {id: '3', place: 'Georgia'},
+    ]
+}
 class SearchInput extends React.Component {
     state = {
-
+        location: [],
+        dataValue: 'nigeria'
+      }
+  
+    onChange = ({ target: { value } }) => {
+      this.setState({ dataValue: value });
     }
+    handleChange = ({ target: { value } }) => {
+      this.setState({ location: value });
+    }
+  
     render() {
-        return (
-            <div className="search-input">
-                <select className="search-select">
-                    <option>Select your city</option>
-                    <option>Lagos</option>
-                    <option>Abuja</option>
-                    <option>Ibadan</option>
-                </select>
-                <select className="search-select">
-                    <option>Select your Location</option>
-                    <option>Lagos</option>
-                    <option>Abuja</option>
-                    <option>Ibadan</option>
-                </select>
-                <input className="search-button" type="button" value="Find Food" />
-            </div>
+      const { dataValue } = this.state;
+      const options = lookup[dataValue];
+      return (
+        <div className="search-input">
+          <select className="search-select" onChange={this.onChange}>
+            <option className='option' value="select">Select Country</option>
+            <option value="nigeria">Nigeria</option>
+            <option value="ghana">Ghana</option>
+            <option value="usa">USA</option>
+          </select>
+          <hr />
+          <select className="search-select2" onChange={this.handleChange}>
+            <option value="select">Select Location</option>
+            {options.map(o => <option key={o.id} value={o.place}>{o.place}</option>)}
+          </select>
+            <input className="search-button" type="button" value="Find Food" />
+            
+        </div>
         )
     }
 }
 
 export default SearchInput
+
+
+
